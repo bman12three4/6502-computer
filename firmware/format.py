95cc01114b666ae6fce8a9666279e250c7dc4e11
Byron Lathi <byron.lathi@student.lovett.org>
2019-11-17 14:12:12 -0600

    Automated programming

--- firmware/format.py
+++ firmware/format.py
@@ -1,5 +1,12 @@
 with open('out.bin', 'rb') as f:						#load binary file
-    hex_content = f.read().encode('hex')					#encode it as hex
-    t = iter(hex_content)			
-    vals= ' '.join(hex_content[i:i+2] for i in range(0, len(hex_content), 2))	# add a space between each byte
-    print("byte data[] = {0x" + vals.replace(" ", ", 0x") + "};")		# format to an arduino array
+	hex_content = f.read().encode('hex')					#encode it as hex
+	t = iter(hex_content)			
+	vals= ' '.join(hex_content[i:i+2] for i in range(0, len(hex_content), 2))	# add a space between each byte
+	eeprom = ("byte data[] = {0x" + vals.replace(" ", ", 0x") + "};")	# format to an arduino array
+	print(eeprom)
+	
+	n = open("nodata.ino", "r")
+	programmer = n.read();
+	p = open("programmer/programmer.ino", "w")
+	p.write(eeprom + "\n" + programmer)
+	p.close()
--- firmware/makebat.bat
+++ firmware/makebat.bat
@@ -1,3 +1,6 @@
-ca65 -l out.lst -o out.hex runram.s65
+ca65 -l out.lst -o out.hex lcdtest2.s65
 ld65 out.hex -o out.bin -m out.map -C firmware.cfg
 python format.py
+arduino-cli compile -b arduino:avr:uno programmer\programmer.ino -o programmer.hex
+arduino-cli upload -i programmer -p COM4 -b arduino:avr:uno
+putty.exe -load "arduino"
